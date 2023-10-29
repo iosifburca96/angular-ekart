0dec08d623bc59762bfbc7c60472a2be97645184
Iosif Burca <iosifburca1@gmail.com>
2023-10-29 17:47:18 +0200

    added styles to the header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ContentComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,9 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    template: '<h3>eKart</h3>',
+    templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-    
+
 }
