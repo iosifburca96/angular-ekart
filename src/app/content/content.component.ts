a5b9bb44a10f7633e0dfbcd1e9ce66cd91daa793
Iosif Burca <iosifburca1@gmail.com>
2023-10-29 17:12:02 +0200

    added components

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-content',
+    template: '<div> Hello, my name is Iosif</div>',
+    styleUrls: ['./content.component.css']
+})
+export class ContentComponent {
+
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    template: '<h3>eKart</h3>',
+    styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+    
+}
