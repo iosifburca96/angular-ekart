460dc06fe50f3f4d7b7bdb733549794e9a460e6c
Iosif Burca <iosifburca1@gmail.com>
2023-11-08 12:14:33 +0200

    string interpolation

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-product-list',
+  selector: 'product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+    product = {
+        name: 'iPhone X',
+        price: 789,
+        color: 'Black',
+        discount: 8.5,
+        inStock: 0
+    }
 
+    getDiscountedPrice() {
+        let discountedPrice: number = this.product.price - (this.product.price * this.product.discount / 100);
+        return Math.floor(discountedPrice);
+    }
 }
